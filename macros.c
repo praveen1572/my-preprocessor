204bf358a303ffcba8a03f0ae7c90fdb9c24bf23
Praveen <praveenkannan1572@gmail.com>
2026-08-20 08:48:05 +0530

    Update macros.c

--- macros.c
+++ macros.c
@@ -5,20 +5,17 @@
 
 int store_macro(char *line){
     int i=8,j=0,k=0;
-
     macro[count]=malloc(50);
     body[count]=malloc(50);
-
+    
     while(line[i]!=' ')
         macro[count][j++]=line[i++];
     macro[count][j]='\0';
-    
     i++;
-
+    
     while(line[i]!='\n'&&line[i]!='\0')
         body[count][k++]=line[i++];
     body[count][k]='\0';
-
     count++;
     return 0;
 }
@@ -29,14 +26,11 @@ int substitute_macro(char *line,FILE *out){
 
     while(line[i]!='\0'){
         if((line[i]>='A'&&line[i]<='Z')||(line[i]>='a'&&line[i]<='z')||line[i]=='_'){
-
             j=0;
-
             while((line[i]>='A'&&line[i]<='Z')||(line[i]>='a'&&line[i]<='z')||(line[i]>='0'&&line[i]<='9')||line[i]=='_')
                 temp[j++]=line[i++];
 
             temp[j]='\0';
-
             for(k=0;k<count;k++){
                 if(strcmp(temp,macro[k])==0)
                     break;
@@ -52,6 +46,5 @@ int substitute_macro(char *line,FILE *out){
             i++;
         }
     }
-
     return 0;
 }
